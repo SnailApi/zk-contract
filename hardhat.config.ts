55dcd4567dc2577aa47712770d4b880348a8ce13
drawrowfly <anonim.megamim@gmail.com>
2023-02-06 11:53:57 -0500

    first commit

---
+++ hardhat.config.ts
@@ -0,0 +1,25 @@
+require("@matterlabs/hardhat-zksync-deploy");
+require("@matterlabs/hardhat-zksync-solc");
+
+module.exports = {
+  zksolc: {
+    version: "1.2.0",
+    compilerSource: "binary",
+    settings: {
+      optimizer: {
+        enabled: true,
+      },
+    },
+  },
+  defaultNetwork: "zkTestnet",
+  networks: {
+    zkTestnet: {
+      url: "https://zksync2-testnet.zksync.dev", // URL of the zkSync network RPC
+      ethNetwork: "goerli", // Can also be the RPC URL of the Ethereum network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
+      zksync: true,
+    },
+  },
+  solidity: {
+    version: "0.8.16",
+  },
+};
